281a041e1a2c5915c7b5debc836486ad02cb1b70
Terence Ro <rowyourboat83@gmail.com>
2017-03-12 15:43:33 -0500

    move data mapping logic to search.js

--- app/components/children/Search.js
+++ app/components/children/Search.js
@@ -9,15 +9,36 @@ var helpers = require('../utils/helpers');
 
 // Create the search form component
 var Search = React.createClass({
+    getInitialState: function () {
+        return {
+            result: []
+        };
+    },
     // set the generic state
     componentDidUpdate: function () {
-        helpers.runQuery(this.state.term).then( (data) => {
-            if (data !== this.state.results) {
-                console.log("dude", data);
+        helpers.runQuery(this.state.term).then(function (response)  {
+            
+                console.log("dude", response);
 
-                this.setState({ result: data });
-            }
-        });
+                
+                console.log(response.data.response.docs);
+                var results = [];
+                
+                // iterate over the API response and assign data to variables.
+                for (var {headline: {main: title}, pub_date: date, web_url: link} of response.data.response.docs) {
+                    
+                    var articleObject = { title, date, link };
+                    
+                    var articles = <div className="row"><div className="card-panel teal"><ul><span className="white-text"><li>{articleObject.title}</li><li>{articleObject.date}</li><a href={articleObject.link}>{articleObject.link}</a></span><hr /></ul></div></div>
+                    
+                    results.push(articles);
+                    console.log('>', articles);
+                }
+                console.log('data', results);
+
+            this.setState({ result: results });
+            
+        }.bind(this));
     },
     getInitialState: function () {
         return { term: '' };
@@ -41,6 +62,11 @@ var Search = React.createClass({
         
     },
     render: function () {
+        if (this.state.result){
+        var article = this.state.result.map(function (article, index) {
+            return (<li key={index}>{article}<a href='' onClick={() => helpers.postHistory(article)} className='btn waves-effect waves-light' type='submit'>Save</a></li>);
+        });
+        }
         return (
             <div>
                 <div className="row">
@@ -53,8 +79,10 @@ var Search = React.createClass({
                         <br />
                         <a href='#/Results' className='btn waves-effect waves-light' type='submit'>Submit</a>
                     </form>
-                   
-                    {this.state.result}
+        
+                   <ul>
+                    {article}
+                    </ul>
                 </div>
             </div>
         );
--- app/components/utils/helpers.js
+++ app/components/utils/helpers.js
@@ -15,24 +15,9 @@ var helper = {
         var queryURL = 'https://api.nytimes.com/svc/search/v2/articlesearch.json?api-key=' + nytKey + '&q=' + topic;
         // call GET method using axios
         return axios.get(queryURL).then(function (response) {
-            // If GET gets a response, return that as 'results'
+            // If GET gets a response, return response
             if (response.data.response.docs) {
-                console.log(response.data.response.docs);
-                var results = [];
-                var articles;
-                // iterate over the API response and assign data to variables.
-                for (var {headline: {main: title}, pub_date: date, web_url: link} of response.data.response.docs) {
-                    // console.log(title);
-                    var articleArray = { title, date, link };
-                    //  articles = articleArray.map((article) =>
-                    articles = <div className="row"><div className="card-panel teal"><ul><span className="white-text"><li>{articleArray.title}</li><li>{articleArray.date}</li><a href={articleArray.link}>{articleArray.link}</a></span><hr /></ul></div><a href='#' onClick={handleClick} className='btn waves-effect waves-light' type='submit'>Save</a></div>
-                    // );
-                    results.push(articles);
-                    console.log('>', articles);
-                }
-                console.log('data', results);
-
-                return results;
+                return response;
             } else {
                 return console.log('error no articles');
             }
